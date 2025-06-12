87a30269a70cf3b3d35224cc86ecda674ebe3b5b
Vmp099 <viniciusmendespavesi@gmail.com>
2025-06-12 17:07:56 -0300

    COMMIT - LUCAS

---
+++ app/javascript/application.js
@@ -0,0 +1,22 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+// // application.js
+// function playMusic() {
+//     const audio = document.getElementById("audio");
+//     audio.play();
+// }
+
+// let currentSlide = 0;
+
+// function moveSlide(step) {
+//     const slides = document.querySelectorAll('.carousel-images img');
+//     currentSlide = (currentSlide + step + slides.length) % slides.length; // Garantir que o índice seja circular
+//     document.querySelector('.carousel-images').style.transform = translateX(-${currentSlide * 100}%);
+// }
+
+// setInterval(() => {
+//     moveSlide(1); // Avança para o próximo slide
+// }, 3000);
+
